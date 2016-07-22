bc216d1d24bcc487dc2104b7d29b4bc8efd6e933
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-07-21 21:43:21 -0400

    added a nav drawer to the app bar with some selections

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import * as colors from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 
 const lightCustomMuiTheme = getMuiTheme({
     palette: {
@@ -14,11 +16,37 @@ const lightCustomMuiTheme = getMuiTheme({
 });
 
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            drawerOpen: false
+        };
+    }
+
+    navMenuClick = () => this.setState({ drawerOpen: !this.state.drawerOpen });
+    closeDrawer = () => this.setState({ drawerOpen: false });
+
     render() {
         return (
             <MuiThemeProvider muiTheme={lightCustomMuiTheme}>
-                <AppBar
-                    title="Yash Kulshrestha" />
+                <div>
+                    <AppBar
+                        title="Yash Kulshrestha"
+                        onLeftIconButtonTouchTap={this.navMenuClick} />
+
+                    <Drawer
+                        open={this.state.drawerOpen}
+                        docked={false}
+                        onRequestChange={this.closeDrawer}>
+                        <MenuItem>Work Experience</MenuItem>
+                        <MenuItem>Volunteering</MenuItem>
+                        <MenuItem>Experiments</MenuItem>
+                        <MenuItem>About</MenuItem>
+                        <MenuItem>Contact</MenuItem>
+                    </Drawer>
+                </div>
             </MuiThemeProvider>
         );
     }
