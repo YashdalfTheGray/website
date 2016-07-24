b325a13c2226f360add4d80dab3ac1b277a83e51
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-07-23 20:06:15 -0400

    updated for the display logic

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,8 @@ import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
+import WorkExperienceList from './components/WorkExperienceList';
+
 const lightCustomMuiTheme = getMuiTheme({
     palette: {
         primary1Color: colors.blue500,
@@ -21,14 +23,38 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            drawerOpen: false
+            drawerOpen: false,
+            section: ''
         };
     }
 
     navMenuClick = () => this.setState({ drawerOpen: !this.state.drawerOpen });
     closeDrawer = () => this.setState({ drawerOpen: false });
+    displayWorkExperience = () => this.setState({ drawerOpen: false, section: 'work-experience' });
+    displayVolunteerWork = () => this.setState({ drawerOpen: false, section: 'volunteer-work' });
+    displayExperiments = () => this.setState({ drawerOpen: false, section: 'experiments' });
+    displayAbout = () => this.setState({ drawerOpen: false, section: 'about' });
+    displayContact = () => this.setState({ drawerOpen: false, section: 'contact' });
 
     render() {
+        var viewToDisplay;
+
+        switch(this.state.section) {
+            case 'work-experience':
+                viewToDisplay = <WorkExperienceList />
+                break;
+            case 'volunteer-work':
+                break;
+            case 'experiments':
+                break;
+            case 'about':
+                break;
+            case 'contact':
+                break;
+            default:
+                viewToDisplay = <div>How did we get here?</div>
+        }
+
         return (
             <MuiThemeProvider muiTheme={lightCustomMuiTheme}>
                 <div>
@@ -36,15 +62,32 @@ class App extends React.Component {
                         title="Yash Kulshrestha"
                         onLeftIconButtonTouchTap={this.navMenuClick} />
 
+                    {viewToDisplay}
+
                     <Drawer
                         open={this.state.drawerOpen}
                         docked={false}
                         onRequestChange={this.closeDrawer}>
-                        <MenuItem>Work Experience</MenuItem>
-                        <MenuItem>Volunteering</MenuItem>
-                        <MenuItem>Experiments</MenuItem>
-                        <MenuItem>About</MenuItem>
-                        <MenuItem>Contact</MenuItem>
+                        <MenuItem
+                            onTouchTap={this.displayWorkExperience}>
+                            Work Experience
+                        </MenuItem>
+                        <MenuItem
+                            onTouchTap={this.displayVolunteerWork}>
+                            Volunteer Work
+                        </MenuItem>
+                        <MenuItem
+                            onTouchTap={this.displayExperiments}>
+                            Experiments
+                        </MenuItem>
+                        <MenuItem
+                            onTouchTap={this.displayAbout}>
+                            About
+                        </MenuItem>
+                        <MenuItem
+                            onTouchTap={this.displayContact}>
+                            Contact
+                        </MenuItem>
                     </Drawer>
                 </div>
             </MuiThemeProvider>
